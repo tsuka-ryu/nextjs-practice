cfc3e92f9c64fee4a25ea08fbf6fcbdb81fe12af
tsukaryu <drgon8otsuka@gmail.com>
2025-05-05 21:21:55 +0900

    fix: ビルド通す

--- src/app/dynamic-io-suspense/page.tsx
+++ src/app/dynamic-io-suspense/page.tsx
@@ -3,6 +3,8 @@ import { FetchComponents } from "./_components/fetch-component";
 
 // dynamic-ioを試す
 // https://zenn.dev/akfm/books/nextjs-basic-principle/viewer/part_3_dynamicio
+
+// dynamic-ioフラグを無効化してもビルド通るし、普通にsuspenseの動きになるな、これはstreaming ssrか
 export default async function DynamicIoTest() {
   return (
     <div>
